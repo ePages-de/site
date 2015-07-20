ae3b13e5ab066837163f1a3ec5617e7db6820c1d
Lorenz Kitzmann <lorenz@lorenzk.de>
2015-07-20 11:32:01 +0200

    fix ci.sh

--- ci.sh
+++ ci.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-set -ex
-PORT=4321 npm start &
+set -x
+gulp build
+
+PORT=4321 node index.js &
 NODE_PID=$!
 
 node proxy.js &
 PROXY_PID=$!
+
 ./node_modules/karma/bin/karma start karma.conf.coffee --single-run
 
+pkill -P $NODE_PID
 kill $NODE_PID
 kill $PROXY_PID
